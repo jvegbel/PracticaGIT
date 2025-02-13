9b37a0adaaf698c679a1af7ef2e0ae34cdce9059
info02 <info02@info02>
2025-02-13 00:32:24 +0000

    Prueba inicial

---
+++ src/practicagit/PracticaGIT.java
@@ -0,0 +1,17 @@
+
+package practicagit;
+
+/**
+ *
+ * @author info02
+ */
+public class PracticaGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Esto es una prueba");
+    }
+    
+}
